b28d68bb9e7aba5a110184c8e777dc0ee71dcb91
ekeemmanuel <emmazy100@gmail.com>
2022-08-26 18:07:13 +0100

    new functions

--- index.js
+++ index.js
@@ -1,101 +1,26 @@
 let passwordText = document.getElementById("pass-description");
-let characters = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-  "0",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "~",
-  "`",
-  "!",
-  "@",
-  "#",
-  "$",
-  "%",
-  "^",
-  "&",
-  "*",
-  "(",
-  ")",
-  "_",
-  "-",
-  "+",
-  "=",
-  "{",
-  "[",
-  "}",
-  "]",
-  ",",
-  "|",
-  ":",
-  ";",
-  "<",
-  ">",
-  ".",
-  "?",
-  "/",
-];
+let characters =
+  "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+[]{}?><";
+let pass = document.getElementById("pass");
 
 function generateRandom() {
-  let random = Math.floor(Math.random() * 100000000000000);
-  let ranchar = random * characters.length;
-  passwordText.innerHTML = random;
-  console.log(ranchar);
+  let passwordLength = 15;
+  let password = "";
+  for (let i = 0; i < passwordLength; i++) {
+    password += characters[Math.floor(Math.random() * passwordLength)];
+  }
+  pass.value = password;
+
+  return password;
+
+  //   let random = Math.floor(Math.random() * 100000000000000);
+  //   let ranchar = random * characters.length;
+  //   passwordText.innerHTML = random;
+  //   console.log(ranchar);
+}
+
+function copyPass() {
+  if (pass != "") {
+    navigator.clipboard.writeText(pass.value);
+  }
 }
