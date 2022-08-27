a7697b227f704a12cbd3e5113cb82a45957a9ae9
ekeemmanuel <emmazy100@gmail.com>
2022-08-27 05:48:54 +0100

    password lenght

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ let passwordText = document.getElementById("pass-description");
 let characters =
   "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+[]{}?><";
 let pass = document.getElementById("pass");
+let passwordLen = document.getElementById("passlen");
 
 function generateRandom() {
-  let passwordLength = 15;
+  let passwordLength = passwordLen.value;
   let password = "";
+
   for (let i = 0; i < passwordLength; i++) {
     password += characters[Math.floor(Math.random() * characters.length)];
   }
